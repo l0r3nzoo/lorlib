9d8e5a07f73e8f0f67090138b8f1b1f7c3ea86ca
loren <lorenbhanu@gmail.com>
2025-12-30 01:05:11 +0530

    vector library

---
+++ vector.c
@@ -0,0 +1,102 @@
+#include "vector.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INITIAL_CAP 10
+
+vector *create_vector(size_t ts) {
+  vector *v = malloc(sizeof(vector));
+  if (v == NULL) {
+    return NULL;
+  }
+  v->data = malloc(ts * INITIAL_CAP);
+  if (v->data == NULL) {
+    free(v);
+    return NULL;
+  }
+  v->capacity = INITIAL_CAP;
+  v->count = 0;
+  v->type_size = ts;
+  return v;
+}
+
+int vec_push(vector *v, void *i) {
+  if (v == NULL) {
+    return 1;
+  }
+  if (v->count == v->capacity) {
+    size_t temp_cap = v->capacity + INITIAL_CAP;
+    void *temp_block = realloc(v, temp_cap * v->type_size);
+    if (temp_block == NULL) {
+      return 1;
+    }
+    v->capacity = temp_cap;
+    v->data = temp_block;
+  }
+  memcpy((char *)v->data + v->type_size * v->count, i, v->type_size);
+  v->count++;
+  return 0;
+}
+
+int vec_remove(vector *v, void *i) {
+  if (v == NULL) {
+    return 1;
+  }
+  for (size_t j = 0; j < v->count; j++) {
+    if (&v->data[j] == i) {
+      for (size_t k = j + 1; k < v->count; k++) {
+        memcpy((char *)v->data + v->type_size * (k - 1),
+               (char *)v->data + v->type_size * k, v->type_size);
+      }
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int vec_remove_at(vector *v, size_t i) {
+  if (i >= v->count) {
+    return 1;
+  }
+  for (int k = i + 1; k < v->count; k++) {
+    memcpy((char *)v->data + v->type_size * (k - 1),
+           (char *)v->data + v->type_size * k, v->type_size);
+  }
+  return 0;
+}
+
+int vec_clear(vector *v) {
+  void *temp_block = realloc(v->data, v->type_size * INITIAL_CAP);
+  if (temp_block == NULL) {
+    return 1;
+  }
+  v->data = temp_block;
+  v->capacity = INITIAL_CAP;
+  v->count = 0;
+  return 0;
+}
+
+size_t vec_index_of(vector *v, void *i) {
+  if (v == NULL) {
+    return SIZE_MAX;
+  }
+  for (int j = 0; j < v->count; j++) {
+    if (&v->data[j] == i) {
+      return j;
+    }
+  }
+  return SIZE_MAX;
+}
+
+int free_vector(vector **v){
+    if(*v==NULL){
+        return 1;
+    }
+    free((*v)->data);
+    free(*v);
+    *v=NULL;
+    return 0;
+}
---
+++ vector.h
@@ -0,0 +1,55 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct vector {
+  void *data;
+  size_t type_size;
+  size_t count;
+  size_t capacity;
+} vector;
+
+/// Initializes vector, Index starts from 0, capacity is set internally
+/// @param ts Size of the type for the vector such as int float char
+/// @return pointer to vector on SUCCESS, NULL on FAILURE
+vector *create_vector(size_t ts);
+
+/// Pushes new item to the vector
+/// @param v Pointer to the vector
+/// @param i Pointer to the item to be pushed to vector
+/// @return 0 on SUCCESS, 1 on FAILURE
+int vec_push(vector *v, void *i);
+
+/// remove item from the vector
+/// @param v Pointer to the vector
+/// @param i Pointer to the item to be removed from the vector
+/// @return 0 on SUCCESS, 1 on FAILURE
+int vec_remove(vector *v, void *i);
+
+/// remove item at specific index
+/// @param v Pointer to the vector
+/// @param i index of the item to be removed from the vector
+/// @return 0 on SUCCESS, 1 on FAILURE
+int vec_remove_at(vector *v, size_t i);
+
+/// Clear the items that the vector holding and reset the count to 0
+/// @param v Pointer to the vector
+/// @return 0 on SUCCESS, 1 on FAILURE
+int vec_clear(vector *v);
+
+/// Index of the item
+/// @param v Pointer to the vector
+/// @param i Pointer to the item
+/// If the item is found, returns its index (0-based)
+/// If the item is not found, returns `SIZE_MAX`
+size_t vec_index_of(vector *v, void *i);
+
+/// free the vector memory
+/// @param v Pointer to the Pointer to the vector
+///
+/// This function frees the memory allocated for the vector itself
+/// and also frees the memory pointed to by the vector's data field.
+/// After calling this, the vector pointer is set to NULL.
+/// @return 0 on SUCCESS, 1 on FAILURE
+int free_vector(vector **v);
+
+#endif

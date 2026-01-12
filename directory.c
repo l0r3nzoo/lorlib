e3aed07e36571a2fbdfb771a4e7f53c11c7e715d
l0r3nz0 <lorenbhanu+git@proton.me>
2026-01-12 15:57:09 +0530

    added linux list files of a directory support

--- directory.c
+++ directory.c
@@ -2,24 +2,47 @@
 #include <sys/stat.h>
 #ifdef _WIN32
 #include <direct.h>
+#else 
+#include <dirent.h>
 #endif
 #include <sys/stat.h>
 
 void dir_create(const char *name) {
 #ifdef __linux__
-  mkdir(name, 0755);
+	mkdir(name, 0755);
 #else
-  _mkdir(name);
+	_mkdir(name);
 #endif
 }
 
 void dir_create_recursive(char *name) {
-  char *path = strtok(name, "/\\");
-  char buffer[1024]="";
-  while (path != NULL) {
-    strcat(buffer, path);
-    dir_create(buffer);
-    strcat(buffer, "/");
-    path = strtok(NULL, "/\\");
-  }
+	char *path = strtok(name, "/\\");
+	char buffer[1024]="";
+	while (path != NULL) {
+		strcat(buffer, path);
+		dir_create(buffer);
+		strcat(buffer, "/");
+		path = strtok(NULL, "/\\");
+	}
+}
+
+void dir_list_files(char* directory_path,void (*file_handle)(char* file_path)){
+	DIR* direct=opendir(directory_path);
+	if(direct==NULL){
+		return;
+	}
+	struct dirent* infile;
+	while((infile=readdir(direct))){
+		struct stat buffer;
+		int status;
+		status=stat(infile->d_name,&buffer);
+		if(status==-1){
+
+		}
+		else if(buffer.st_mode & S_IFREG){
+			file_handle(infile->d_name);
+		}
+
+	}
+
 }
--- directory.h
+++ directory.h
@@ -3,5 +3,6 @@
 
 void dir_create(const char *name);
 void dir_create_recursive(char *name);
+void dir_list_files(char* directory_path,void (*file_handle)(char* file_path));
 
 #endif
